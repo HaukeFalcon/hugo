935afddb394f40791534ec60271da2d024acb235
HaukeFalcon <156836594+HaukeFalcon@users.noreply.github.com>
2024-01-17 14:25:25 -0600

    Add files via upload

--- FUNNEL.py
+++ FUNNEL.py
@@ -7,5 +7,5 @@ pip.main(["install","openpyxl"])
 
 st.title("JAJA CREDITOS ATORADOS EN PROCESO")
 #df=pd.read_excel('PROCESOS_PIVOTE.xlsx')
-f=pd.read_csv('PROCESOS_PIVOTE.csv')
+df=pd.read_csv('PROCESOS_PIVOTE.csv')
 st.write(df)
